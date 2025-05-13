b1b0aa35c11c84f4a2e8eacb20541b02dc410c5a
Harsh <harsh20050924@gmail.com>
2025-05-13 18:06:17 +0530

    Project structured ready ,venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Harsh Sharma",
+    author_email="harsh20050924@gmail.com",
+    packages=find_packages()
+)
